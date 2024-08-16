eae2ba81c52f28fc71e5f9058fa076f114e1a8b1
hrmrakib <hrmrakibs@gmail.com>
2024-08-16 17:20:20 +0600

    add Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import ButtonGradient from "./../public/assets/svg/ButtonGradient";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
 export default function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+      <div className='pt-[4.75rem] lg:pt-[5.25rem]'></div>
       <ButtonGradient />
     </>
   );
